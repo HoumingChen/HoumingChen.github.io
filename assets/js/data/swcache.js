caa9bb2ba2271a204299f51a9dc4d4a72d3a62df
houmingc <houmingc@umich.edu>
2021-12-27 03:03:19 -0500

    update

---
+++ assets/js/data/swcache.js
@@ -0,0 +1,60 @@
+const resource = [
+
+  /* --- CSS --- */
+  '/HoumingChen.github.io/assets/css/style.css',
+
+  /* --- JavaScripts --- */
+  
+  '/HoumingChen.github.io/assets/js/dist/home.min.js',
+  '/HoumingChen.github.io/assets/js/dist/page.min.js',
+  '/HoumingChen.github.io/assets/js/dist/post.min.js',
+  '/HoumingChen.github.io/assets/js/dist/categories.min.js',
+  '/HoumingChen.github.io/assets/js/data/search.json',
+  '/HoumingChen.github.io/app.js',
+  '/HoumingChen.github.io/sw.js',
+
+  /* --- HTML --- */
+  '/HoumingChen.github.io/index.html',
+  '/HoumingChen.github.io/404.html',
+  
+    '/HoumingChen.github.io/categories/',
+  
+    '/HoumingChen.github.io/tags/',
+  
+    '/HoumingChen.github.io/archives/',
+  
+    '/HoumingChen.github.io/about/',
+  
+
+  /* --- Favicons --- */
+  
+
+  '/HoumingChen.github.io/assets/img/favicons/android-chrome-192x192.png',
+  '/HoumingChen.github.io/assets/img/favicons/android-chrome-512x512.png',
+  '/HoumingChen.github.io/assets/img/favicons/apple-touch-icon.png',
+  '/HoumingChen.github.io/assets/img/favicons/favicon-16x16.png',
+  '/HoumingChen.github.io/assets/img/favicons/favicon-32x32.png',
+  '/HoumingChen.github.io/assets/img/favicons/favicon.ico',
+  '/HoumingChen.github.io/assets/img/favicons/mstile-150x150.png',
+  '/HoumingChen.github.io/assets/img/favicons/site.webmanifest',
+  '/HoumingChen.github.io/assets/img/favicons/browserconfig.xml'
+
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+  
+
+  '',
+
+  'fonts.gstatic.com',
+  'fonts.googleapis.com',
+  'cdn.jsdelivr.net',
+  'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [
+  
+];
+
