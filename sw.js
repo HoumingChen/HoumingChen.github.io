cfc7dc5119d2d9a5fc0157e71d7751999fc6ed64
HoumingChen <houmingc@umich.edu>
2022-01-04 08:24:44 -0500

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220104.0811';
+const cacheName = 'chirpy-20220104.0823';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
